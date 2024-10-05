62a50a49ac33013366ceefdb4e9976f39f32e7e6
ShinjiMC <nayapbrau@gmail.com>
2024-10-05 12:24:41 -0500

    Fix Issues Animation Rigging

--- Assets/Scripts/SparkOnCollision.cs
+++ Assets/Scripts/SparkOnCollision.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 public class SparkOnCollision : MonoBehaviour
 {
     public ParticleSystem sparks; // Asigna aquí el Particle System de chispas
